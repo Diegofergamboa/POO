d9047ed364ede77b7b431613724e05b6c4656ea3
Diego Ortiz Gamboa <diegofergamboa@hotmail.es>
2020-10-07 14:04:30 -0500

    Anotaciones sobre que son las clases y las iteraciones en un ejemplo

---
+++ Anotaciones.py
@@ -0,0 +1,45 @@
+
+#Las clases son aquellas que nos permiten realizar representaciones
+#de las abstracciones de la realidad en objetos en la programaciòn
+
+
+
+#
+def __init__(self, saluda, come):
+    self.saluda(saluda_otrapersona)
+    self.come(alimento_come)
+        
+def saluda(self, otra_persona):
+    return f'Hola {otra_persona}, me llamo {self.nombre}.'
+
+#Uso
+
+david = Persona('David', 35):
+erika = Persona('Erika', 32):
+    
+#ejemplo
+
+#primero se mira si se puede heredar a otra clase
+
+class nombre_de_la_clase(super_clase):
+    
+    
+
+        
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    Persona():
+    
