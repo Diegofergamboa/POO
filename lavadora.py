7d863b45dd2f8131327bbc59ab4ce343571fdc66
Diego Ortiz Gamboa <diegofergamboa@hotmail.es>
2020-10-09 13:35:31 -0500

    Se han arreglado todo lo concerniente a la lavadora, el problema consistia en que no habia ninguna identacion debida sumado al problema de que los metodos no estaban especificados, ya fue debugeado el programa

--- lavadora.py
+++ lavadora.py
@@ -2,35 +2,40 @@
 
 #Clase para entender mas a profundidad las abstracciones
 #Definicion de la clase
-class Lavadora():
+
+
+class Lavadora:
         
     def __init__(self):
         pass
     
     def lavar(self):
-        self.llenado = llenado
-        self.lavado = lavado
-        self.centrifugado = centrifugado
-        self.enjuaga = enjuaga
-        self.seca = seca
+        self._llenado()
+        self._lavado()
+        self._centrifugado()
+        self._enjuaga()
+        self._seca()
                 
 #Proceso de identidad
+
+    def _lavar(self):
+        print('Esta comenzando el proceso de lavado')
         
     def _llenado(self):
-        print('')
+        print('Llenando la lavadora')
             
     def _lavado(self):
-        print('el ha empezado')
+        print('el lavado ha empezado')
             
     def _centrifugado(self):
-        print('el ha comenzado')
+        print('el centrifugado ha comenzado')
            
     def _enjuaga(self):
-        print('Esta centrifugando la maquina')
+        print('La maquina esta enjuagando')
         
     def _seca(self):
         print('Esta secando la maquina')
-    
+
 if __name__ == '__main__':
     lavadora = Lavadora()
     lavadora.lavar()
