f08b470fd4bfde781dbc6c3724a29f1c24ad8f94
Diego Ortiz Gamboa <diegofergamboa@hotmail.es>
2020-11-15 21:40:43 -0500

    Primer logaritmo de ordenamiento

---
+++ ordenamientodeburbuja.py
@@ -0,0 +1,27 @@
+'''
+    Vamos a intercambiar los valores que hay en la lista hasta que queden ordenado
+'''
+
+
+def ordenamiento_de_burbuja(lista):
+    n = len(lista)
+    
+    for i in range(n):
+        for j in range(0, n - i - 1):
+            
+            if lista[j] > lista[j + 1]:
+                lista[j], lista[j + 1] = lista[j + 1], lista[j]
+            
+     return lista       
+            
+if __name__ == '__main__':
+    tamano_de_lista = int(input('De que tamano sera la lista? '))
+   
+#Funcion sorted ordena la lista
+
+    lista = [random.randint[0, 100] for i in range(tamano_de_lista)]
+
+    print(lista)
+    
+    lista_ordenada = ordenamiento_de_burbuja(lista)
+    print(lista_ordenada)
